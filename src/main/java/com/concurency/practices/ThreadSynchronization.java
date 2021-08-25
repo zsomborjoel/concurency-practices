6cc4cd107f31e27e65d3462cadc1191268bd5602
zsomborjoel <zsomborjoel@gmail.com>
2021-08-25 19:29:47 +0200

    ThreadSync

---
+++ src/main/java/com/concurency/practices/ThreadSynchronization.java
@@ -0,0 +1,48 @@
+package com.concurency.practices;
+
+import java.util.Scanner;
+
+class Processor extends Thread {
+
+    // volatile prevents caching variables
+    private volatile boolean running = true;
+
+    // not expected other thread will modify running this thread
+    public void run() {
+
+        while(running) {
+            System.out.println("Hello");
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void shutdown() {
+        running = false;
+    }
+}
+
+public class ThreadSynchronization {
+
+    /**
+     * Two threading running here. Main is one thread and other thread is
+     * Porcessor thread after started.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        processor.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        // gracefully shutdown by main thread
+        processor.shutdown();
+    }
+
+}
